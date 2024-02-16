76f92dc530c2ac064c7dcef40346089ac3e7265c
GitUser-3-2-3 <parthsrivastav.00@gmail.com>
2024-02-16 19:31:21 +0530

    Inheritance

---
+++ src/Lecture3/BoxWeight.java
@@ -0,0 +1,40 @@
+package Lecture3;
+
+/**
+ * BoxWeight
+ */
+public class BoxWeight extends Box {
+    double weight;
+
+    public BoxWeight() {
+        this.weight = -1;
+    }
+
+    public BoxWeight(BoxWeight other) {
+        super(other);
+        weight = other.weight;
+    }
+
+    public BoxWeight(double l, double h, double w, double weight) {
+        super(l, h, w); // used to call the parent class constructor
+        // used to initialize values present in the parent class constructor
+
+        // refers to the weight variable in this class
+        System.out.println(this.weight);
+
+        // calls the parent class for weight variable
+        System.out.println(super.weight);
+        this.weight = weight;
+    }
+
+    public BoxWeight(double l, double h, double weight) {
+        if (weight < 0) {
+            weight = -1;
+        }
+        // if super is not mentioned it calls the default constructor of the parent class
+        this.weight = weight;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("BoxWeight");
+        }
+    }
+}
---
+++ src/Lecture3/Inheritance.java
@@ -0,0 +1,10 @@
+package Lecture3;
+
+/**
+ * Inheritance
+ */
+public class Inheritance {
+    public static void main(String[] args) {
+        
+    }
+}
