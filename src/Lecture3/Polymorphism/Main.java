18af50c6bbcb603054676629ad6268df42936b85
GitUser-3-2-3 <parthsrivastav.00@gmail.com>
2024-02-17 21:31:42 +0530

    Added Inheritance and Polymorphism

--- src/Lecture3/Polymorphism/Main.java
+++ src/Lecture3/Polymorphism/Main.java
@@ -4,6 +4,7 @@ package Lecture3.Polymorphism;
  * Main
  */
 public class Main {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         Shapes shape = new Shapes();
         Circle circle = new Circle();
